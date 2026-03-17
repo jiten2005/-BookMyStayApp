fd30f3dfae725f6601c7e1c3c1ef7a37d313b03b
jiten2005 <jitenkumarbhuyan123@gmail.com>
2026-03-17 15:28:22 +0530

    Jiten Added UC1

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,26 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This program demonstrates the entry point of the Hotel Booking Management System.
+ * It prints a welcome message, application name, and version details.
+ *
+ * @author Jiten
+ * @version 1.0
+ */
+
+public class BookMyStayApp{
+
+    /**
+     * Main method - Entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("     Welcome to Book My Stay");
+        System.out.println(" Hotel Booking Management System");
+        System.out.println(" Version : 1.0");
+        System.out.println("==================================");
+        System.out.println("Application started successfully.");
+    }
+}
